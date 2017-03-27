4b389912af1fa140ea8d3d0c675782716b9a18dc
Rafic Jouejati <Rafic Jouejati>
2017-03-27 18:59:11 -0400

    Combined the files

---
+++ js/local-storage.js
@@ -0,0 +1,18 @@
+if (window.localstorage) {
+
+	var name = document.getElementById("name");
+	var address = document.getElementById("address");
+
+	name.value = localStorage.getItem("name");
+	address.value = localStorage.getItem("address");
+
+	name.addEventListener("becomeSitterSearch", function() {
+		localStorage.setItem("name", name.value);
+	}, false);
+
+	address.addEventListener("input", function() {
+		localStorage.setItem("address", address.value);
+	}, false);
+
+
+}
